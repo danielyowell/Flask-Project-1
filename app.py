2bd4cd0c8c34766e66fda59678d7599d789220ab
Daniel Yowell <danielyowell@ou.edu>
2023-03-19 19:45:59 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello! This is the main page <h1>HELLO!<h1>"
+
+@app.route("/<name>")
+def hello(name):
+    # x = "<script>alert(\"uh oh, security breach\")</script>"
+    return f"Hello, {escape(name)}!"
+
+@app.route('/projects/')
+def projects():
+    return 'The project page'
+
+@app.route('/about')
+def about():
+    return 'The about page'
+
+if __name__ == "__main__":
+    app.run()
